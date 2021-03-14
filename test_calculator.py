87516136bebd7a33955c9a74978ed7e4e9ef2518
Ben <benlee1218@gmail.com>
2021-03-13 16:04:11 -0800

    tests changes

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,10 @@
-import calculator1
+import calculator
+
 
 class TestCalculator:
-    def test_add(self):
-        assert 6 == calculator1.add(1,5)
 
-        assert 2 == calculator1.subtract(4,2)
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
